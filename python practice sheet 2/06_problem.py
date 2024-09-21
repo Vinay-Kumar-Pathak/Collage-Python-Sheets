edf0bbe732bee1aed1e24c236735fec14215c232
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2024-09-21 23:31:43 +0530

    point in triangle

--- python practice sheet 2/06_problem.py
+++ python practice sheet 2/06_problem.py
@@ -1,35 +1,28 @@
-import math
+def triangle_area(x1, y1, x2, y2, x3, y3):
+    # Calculate the lengths of the sides
+    side1 = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+    side2 = ((x3 - x2)**2 + (y3 - y2)**2)**0.5
+    side3 = ((x1 - x3)**2 + (y1 - y3)**2)**0.5
+    s = (side1 + side2 + side3) / 2
+    area = (s * (s - side1) * (s - side2) * (s - side3))**0.5
+    return area
 
 x= float(input("enter the point x :"))
 y= float(input("enter the point y :"))
 
-# cordinates at center/
-# center_x=0
-# center_y=0
-# pointx=(200,0)
-# pointy=(100,0)
-
-# if :
-#     print(f"point of x and y is inside the triangle {x},{y}")
-# else:
-#     print("not in triangle")
-
-
-def triangle_area(x1, y1, x2, y2, x3, y3):
-    return ((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0)
 
 # area of origianl triangle
 original=triangle_area(0,0,200,0,0,100)
 
-# area of other remaining triangles
+#  area of other remaining triangles
 area1= triangle_area(x,y,200,0,0,100)
 area2= triangle_area(0,0,x,y,0,100)
 area3= triangle_area(0,0,200,0,x,y)
 
 # check the point is in the triangele
 
-if(original == (area1 + area2 + area3)):
-    print("Point is in the triange")
+if(original >= (area1 + area2 + area3)):
+    print("Point is in the triangle")
 else:
-    print("Point is not in the triange")
+    print("Point is not in the triangle")
 
