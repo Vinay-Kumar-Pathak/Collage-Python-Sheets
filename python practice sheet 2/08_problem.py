b474c3e05fe34f3c8ff6790ae696a96131ef9d4c
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2024-09-21 22:26:19 +0530

    hex to decimal

--- python practice sheet 2/08_problem.py
+++ python practice sheet 2/08_problem.py
@@ -1,7 +1,8 @@
-charecter=input(" enter a hex character :").upper
+charecter = input("Enter a hex character (0-9, A-F): ")
+
+decimal_value = int(charecter, 16)  
 
 if charecter in '0123456789ABCDEF':
-     decimal_value = int(charecter, 16)
      print(f"The decimal value of hex character '{charecter}' is {decimal_value}.")
 else:
      print("Invalid input! Please enter a valid hex character (0-9 or A-F).")
